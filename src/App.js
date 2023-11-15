4aea29f9a720ed7f25f54aea8b2e97c4bca4e849
vetox88 <sb.sami.bes@gmail.com>
2023-11-15 21:33:53 +0100

    [moving stuff around]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Header from './ProfileHeader';
-
+import Header from './components/ProfileHeader';
+import PersonalInfo from './components/UserProfile';
+import InterestsSection from './components/Interest';
+import theme from './theme';
 
 function App() {
+  
   return (
-    <div>
-     <Header />
+    <div style={theme.palette.container}>
+      <Header />
+      <PersonalInfo />
+      <InterestsSection />
     </div>
   );
 }
